6557c15a843f3d6e4f4ac80656da6dc79419ba41
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-10-04 01:21:27 +0200

    update

--- populate.sh
+++ populate.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-PAYLOAD="{\"event_type\": \"${EVENT}\", \"client_payload\": {\"repo\": \"${1}\"}}"
+# TODO: check environment variables
+
+PAYLOAD="{\"event_type\": \"issue-center-event\", \"client_payload\": {\"repo\": \"${1}\"}}"
 
 curl -d "$PAYLOAD"                                          \
   -H "Content-Type: application/json"                       \
-  -H "Authorization: token ${{ secrets.ISSUE_CENTER }}"     \
+  -H "Authorization: token ${ISSUE_CENTER_TOKEN}"     \
   -H "Accept: application/vnd.github.everest-preview+json"  \
   "https://api.github.com/repos/${ORG}/${REPO}/dispatches"
